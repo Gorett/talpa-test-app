048a1b668385f16d82642ea52ff49993ae8c8910
Oleksii Chuian <aleksey.chuyan@gmail.com>
2020-01-22 22:18:42 +0200

    [master] mocks added to the macks directory.

---
+++ src/mocks/index.js
@@ -0,0 +1,16 @@
+export const gpsPositionMock = {
+    lat: '51.45657',
+    long: ', 7.01228',
+};
+
+export const sensorMock = {
+    name: 'sensor_123',
+    machine: null
+};
+
+export const machineMock = {
+    id: 'asf12l12as187',
+    name: 'belt_conveyor_2',
+    sensors: [sensorMock],
+    lastKnownPosition: gpsPositionMock,
+};

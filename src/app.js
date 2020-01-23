e7c5b15788ef5a0d4f2d928b5e4604dc4913f439
Oleksii Chuian <aleksey.chuyan@gmail.com>
2020-01-23 17:03:35 +0200

    [master] cors support added.

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import graphqlHTTP from 'express-graphql';
+import cors from 'cors';
 import { makeExecutableSchema } from 'graphql-tools';
 
 import typeDefs from './resources/machines/schema';
@@ -8,9 +9,11 @@ import resolvers from './resources/machines/resolvers';
 const schema = makeExecutableSchema({ typeDefs, resolvers });
 
 const app = express();
+
+app.use(cors());
 app.use('/graphql', graphqlHTTP({
     schema,
     graphql: true,
 }));
 
-app.listen(4201);
+app.listen(4000);

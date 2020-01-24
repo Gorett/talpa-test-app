2cde94f5ba4d47fa090129f1a061d19011d8b6e2
Oleksii Chuian <aleksey.chuyan@gmail.com>
2020-01-24 20:17:44 +0200

    [master] Mocks data updated.

--- src/mocks/index.js
+++ src/mocks/index.js
@@ -1,7 +1,19 @@
-export const gpsPositionMock = {
+export const gpsPositionMock1 = {
     id: 'ask12g12op8sl1',
-    lat: '51.45657',
-    lng: ', 7.01228',
+    lat: '51.45633',
+    lng: '7.01297',
+};
+
+export const gpsPositionMock2 = {
+    id: 'ask12g12op8sl1',
+    lat: '51.45321',
+    lng: '7.01112',
+};
+
+export const gpsPositionMock3 = {
+    id: 'ask12g12op8sl1',
+    lat: '51.45792',
+    lng: '7.01416',
 };
 
 export const sensorMock = {
@@ -14,21 +26,21 @@ export const machineMock1 = {
     id: 'asf12l12as187',
     name: 'belt_conveyor_10',
     sensors: [sensorMock],
-    lastKnownPosition: gpsPositionMock,
+    lastKnownPosition: gpsPositionMock1,
 };
 
 export const machineMock2 = {
     id: 'as5kas207412j',
     name: 'belt_conveyor_12',
     sensors: [sensorMock],
-    lastKnownPosition: gpsPositionMock,
+    lastKnownPosition: gpsPositionMock2,
 };
 
 export const machineMock3 = {
     id: 'as12asf012pkl',
     name: 'dreamed_huge_car_43',
     sensors: [sensorMock],
-    lastKnownPosition: gpsPositionMock,
+    lastKnownPosition: gpsPositionMock3,
 };
 
 export const machineListMock = [
